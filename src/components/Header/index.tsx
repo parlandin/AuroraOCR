18b83346a24fdef349b39d808b04b49d869247b9
parlandin <56051040+parlandin@users.noreply.github.com>
2025-06-02 17:04:46 -0300

    feat: implement Header component with navigation and styling

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,57 @@
+import module from "./header.module.css";
+import { Heart } from "lucide-react";
+
+const Header = () => {
+  return (
+    <header className={module.header}>
+      <section className={module.header__section}>
+        <div className={module.header__logo}>
+          <img
+            className={module.header__logo_image}
+            src="/app-icon.png"
+            alt="Logo"
+          />
+
+          <p className={module.header__logo_text}>AuraOCR</p>
+        </div>
+
+        <nav className={module.header__nav}>
+          <ul className={module.header__nav_list}>
+            <li className={module.header__nav_item}>
+              <a className={module.header__nav_link} href="/#recursos">
+                Recursos
+              </a>
+            </li>
+
+            <li className={module.header__nav_item}>
+              <a className={module.header__nav_link} href="/sobre">
+                Sobre
+              </a>
+            </li>
+
+            <li className={module.header__nav_item}>
+              <a className={module.header__nav_link} href="/faq">
+                FAQ
+              </a>
+            </li>
+
+            <li className={module.header__nav_item}>
+              <a
+                href="https://buymeacoffee.com/parlandim"
+                target="_blank"
+                className={module.header__nav_button}
+              >
+                <span className={module.header__nav_button_text}>
+                  Apoiar o Projeto
+                </span>
+                <Heart className={module.header__nav_button_icon} />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </section>
+    </header>
+  );
+};
+
+export default Header;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "@components/Header";
 
 const IndexPage: React.FC = () => {
-  return <div>home</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default IndexPage;
