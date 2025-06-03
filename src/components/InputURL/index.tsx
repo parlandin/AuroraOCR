0513581d207ceeca7637e4deef8d1e64dfabe5a6
parlandin <56051040+parlandin@users.noreply.github.com>
2025-06-02 22:16:00 -0300

    feat: add InputURL component with form for URL input and image submission

---
+++ src/components/InputURL/index.tsx
@@ -0,0 +1,46 @@
+import module from "./inputURL.module.css";
+import React, { useState } from "react";
+import { Link } from "lucide-react";
+
+export interface InputURLProps {
+  setImage?: (image: string | null) => void;
+}
+const InputURL: React.FC<InputURLProps> = ({ setImage }) => {
+  const [url, setUrl] = useState("");
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUrl(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (setImage && url) {
+      setImage(url);
+      setUrl("");
+    }
+  };
+
+  return (
+    <section className={module.inputURL}>
+      <h2 className={module.inputURL__text}>ou insira uma URL</h2>
+      <form className={module.inputURL__form} onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={url}
+          onChange={handleInputChange}
+          placeholder="https://example.com/imagem.jpg"
+          className={module.inputURL__input}
+        />
+        <button
+          type="submit"
+          className={module.inputURL__submitButton}
+          onClick={handleSubmit}
+        >
+          Carregar URL
+          <Link size={17} className={module.inputURL__submitButton_icon} />
+        </button>
+      </form>
+    </section>
+  );
+};
+export default InputURL;
