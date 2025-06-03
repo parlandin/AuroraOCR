83e8ee626a31a65484de5177cbb4f761fd25b29e
parlandin <56051040+parlandin@users.noreply.github.com>
2025-06-03 17:31:03 -0300

    feat: add ConfirmImage component for image confirmation and extraction functionality

---
+++ src/components/ConfirmImage/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import module from "./confirmImage.module.css";
+import { MessageSquareWarning } from "lucide-react";
+
+interface ConfirmImageProps {
+  zoomMessage?: boolean;
+  isCompleted?: boolean;
+}
+
+const ConfirmImage: React.FC<ConfirmImageProps> = ({
+  zoomMessage,
+  isCompleted,
+}) => {
+  const isDisabled = !isCompleted || zoomMessage;
+
+  return (
+    <div className={module.confirm_image}>
+      {!isCompleted && (
+        <p className={module.confirm_image__waning}>
+          <MessageSquareWarning className={module.confirm_image__icon} />
+          Por favor, selecione a área da imagem que deseja extrair o texto.
+        </p>
+      )}
+      {zoomMessage && (
+        <p className={module.confirm_image__waning}>
+          <MessageSquareWarning className={module.confirm_image__icon} />A
+          imagem foi ampliada. Por favor, selecione novamente a área da imagem
+        </p>
+      )}
+      <button className={module.confirm_image__button} disabled={isDisabled}>
+        Extrair texto da imagem
+      </button>
+    </div>
+  );
+};
+export default ConfirmImage;
