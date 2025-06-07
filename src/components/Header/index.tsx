92849b0ff50575370c9b9e7649c21f137f237507
parlandin <56051040+parlandin@users.noreply.github.com>
2025-06-07 17:54:11 -0300

    feat: add install button for PWA in header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,10 @@
 import module from "./header.module.css";
-import { Heart } from "lucide-react";
+import { Heart, Smartphone } from "lucide-react";
+import { usePWA } from "@hooks/usePWA";
 
 const Header: React.FC = () => {
+  const { isInstallable, installPWA } = usePWA();
+
   return (
     <header className={module.header}>
       <section className={module.header__section}>
@@ -29,6 +32,25 @@ const Header: React.FC = () => {
               </a>
             </li>
 
+            {isInstallable && (
+              <li className={module.header__nav_item}>
+                <button
+                  className={
+                    module.header__nav_button +
+                    " " +
+                    module.header__nav_button_install
+                  }
+                  onClick={installPWA}
+                >
+                  Instalar APP
+                  <Smartphone
+                    size={17}
+                    className={module.header__nav_button_icon}
+                  />
+                </button>
+              </li>
+            )}
+
             <li className={module.header__nav_item}>
               <a
                 href="https://buymeacoffee.com/parlandim"
@@ -38,7 +60,7 @@ const Header: React.FC = () => {
                 <span className={module.header__nav_button_text}>
                   Apoiar o Projeto
                 </span>
-                <Heart className={module.header__nav_button_icon} />
+                <Heart size={17} className={module.header__nav_button_icon} />
               </a>
             </li>
           </ul>
