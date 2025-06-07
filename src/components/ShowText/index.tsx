0ed97ed4f52db56208fde4edc907c22a02135cf0
parlandin <56051040+parlandin@users.noreply.github.com>
2025-06-07 17:01:53 -0300

    refactor: replace wrapper div with SectionContainer for improved structure

--- src/components/ShowText/index.tsx
+++ src/components/ShowText/index.tsx
@@ -2,6 +2,7 @@ import { Copy, CopyCheck } from "lucide-react";
 import module from "./showText.module.css";
 import React, { useState } from "react";
 import Autolinker from "autolinker";
+import SectionContainer from "@components/SectionContainer";
 
 interface ShowTextProps {
   text: string;
@@ -33,36 +34,34 @@ const ShowText: React.FC<ShowTextProps> = ({ text, reference }) => {
   };
 
   return (
-    <div className={module.show_text}>
-      <div className={module.show_text__wrapper}>
-        <section className={module.show_text__header}>
-          <h2 className={module.show_text__header_title}>Texto Reconhecido</h2>
-          <button
-            className={module.show_text__header_button}
-            onClick={handleCopy}
-          >
-            {copied ? (
-              <>
-                <CopyCheck size={17} />
-                copiado
-              </>
-            ) : (
-              <>
-                <Copy size={17} />
-                copiar
-              </>
-            )}
-          </button>
-        </section>
+    <SectionContainer id="texto-reconhecido">
+      <section className={module.show_text__header}>
+        <h2 className={module.show_text__header_title}>Texto Reconhecido</h2>
+        <button
+          className={module.show_text__header_button}
+          onClick={handleCopy}
+        >
+          {copied ? (
+            <>
+              <CopyCheck size={17} />
+              copiado
+            </>
+          ) : (
+            <>
+              <Copy size={17} />
+              copiar
+            </>
+          )}
+        </button>
+      </section>
 
-        <div className={module.show_text__content} ref={reference}>
-          <p
-            className={module.show_text__content_text}
-            dangerouslySetInnerHTML={{ __html: processTextWithLinks(text) }}
-          ></p>
-        </div>
+      <div className={module.show_text__content} ref={reference}>
+        <p
+          className={module.show_text__content_text}
+          dangerouslySetInnerHTML={{ __html: processTextWithLinks(text) }}
+        ></p>
       </div>
-    </div>
+    </SectionContainer>
   );
 };
 
